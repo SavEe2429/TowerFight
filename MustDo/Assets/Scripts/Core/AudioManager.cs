9df9287c0e2a6e528b98da400e21b04813e8b380
SavEe2429 <nongsafgml@gmail.com>
2026-03-29 10:19:51 +0700

    test rebase

--- MustDo/Assets/Scripts/Core/AudioManager.cs
+++ MustDo/Assets/Scripts/Core/AudioManager.cs
@@ -5,9 +5,8 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
-
     // Update is called once per frame
     void Update()
     {
--- MustDo/Assets/Scripts/Core/GameManager.cs
+++ MustDo/Assets/Scripts/Core/GameManager.cs
@@ -5,7 +5,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // int game = GetComponent
     }
 
     // Update is called once per frame
